e8d9788d0593dd6aef3d81466deaaf735a415fc9
Karman Singh <t.karman.singh@gmail.com>
2026-07-29 02:48:42 +0530

    style: add tabs after fullstops in marquee and slow to 70s

--- src/components/retro/FooterMarquee.tsx
+++ src/components/retro/FooterMarquee.tsx
@@ -2,11 +2,11 @@
 import React from 'react';
 
 export function FooterMarquee() {
-  const lyrics = "Almost heaven, West Virginia. Blue Ridge Mountains Shenandoah River. Life is old there. Older than the trees. Younger than the mountains. Growin like a breeze Country Roads, take me home. To the place I belong West Virginia, mountain momma. Take me home, country roads.";
+  const lyrics = "Almost heaven, West Virginia.\tBlue Ridge Mountains Shenandoah River.\tLife is old there.\tOlder than the trees.\tYounger than the mountains.\tGrowin like a breeze Country Roads, take me home.\tTo the place I belong West Virginia, mountain momma.\tTake me home, country roads.";
 
   return (
     <div className="w-[80%] md:w-1/3 mx-auto overflow-hidden select-none py-[20px] md:py-[30px] flex">
-      <div className="flex whitespace-nowrap font-kalam text-[18px] md:text-[22px] tracking-wide text-[#3f3f2e] animate-marquee">
+      <div className="flex whitespace-pre font-kalam text-[18px] md:text-[22px] tracking-wide text-[#3f3f2e] animate-marquee">
         <span className="pr-[40px]">🎶 {lyrics} 🎶</span>
         <span className="pr-[40px]">🎶 {lyrics} 🎶</span>
       </div>
