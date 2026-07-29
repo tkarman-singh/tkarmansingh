8365fbc0ecf3a0eb80eebbb205eaf75b07141e6a
Karman Singh <t.karman.singh@gmail.com>
2026-07-29 19:26:46 +0530

    feat: add repeating left and right borders using ka-icon.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,26 @@ import { FooterMarquee } from "@/components/retro/FooterMarquee";
 export default function Home() {
   return (
     <main className="relative w-full overflow-hidden selection:bg-[#df7a3e] selection:text-white pb-[70px]">
+      {/* Left Decorative Border */}
+      <div 
+        className="absolute top-0 left-0 bottom-0 w-[30px] md:w-[50px] z-50 pointer-events-none" 
+        style={{ 
+          backgroundImage: 'url(/ka-icon.png)', 
+          backgroundRepeat: 'repeat-y', 
+          backgroundSize: '100% auto' 
+        }}
+      />
+      
+      {/* Right Decorative Border */}
+      <div 
+        className="absolute top-0 right-0 bottom-0 w-[30px] md:w-[50px] z-50 pointer-events-none" 
+        style={{ 
+          backgroundImage: 'url(/ka-icon.png)', 
+          backgroundRepeat: 'repeat-y', 
+          backgroundSize: '100% auto' 
+        }}
+      />
+
       <Navbar />
       <HeroSection />
       <ProfileSection />
