e0b210bf37fae33805bdf07bf2afe5ff4e27cdfc
Karman Singh <t.karman.singh@gmail.com>
2026-07-29 22:53:19 +0530

    feat: cycle through 4 distinct icons in side borders

--- src/components/retro/SideBorders.tsx
+++ src/components/retro/SideBorders.tsx
@@ -5,17 +5,24 @@ export function SideBorders() {
   // We don't use overflow-hidden on the container so the slide-out text can overflow sideways.
   const icons = Array.from({ length: 200 });
 
+  const iconData = [
+    { src: '/ka-icon.png', text: 'karman' },
+    { src: '/pizza-icon.png', text: 'pizza addict' },
+    { src: '/pointer-icon.png', text: 'solving problems (of life)' },
+    { src: '/mic-icon.png', text: 'might be a rockstar' },
+  ];
+
   return (
     <>
       {/* Left Border */}
       <div className="absolute top-0 left-0 w-[50px] md:w-[75px] z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => {
-          const isKa = i % 2 === 0;
+          const currentIcon = iconData[i % iconData.length];
           return (
             <div key={`left-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
-              <img src={isKa ? "/ka-icon.png" : "/pizza-icon.png"} alt="" className="w-full h-full object-contain relative z-10" />
+              <img src={currentIcon.src} alt="" className="w-full h-full object-contain relative z-10" />
               <span className="absolute top-1/2 left-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 -translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none whitespace-nowrap">
-                {isKa ? 'karman' : 'pizza addict'}
+                {currentIcon.text}
               </span>
             </div>
           );
@@ -25,12 +32,12 @@ export function SideBorders() {
       {/* Right Border */}
       <div className="absolute top-0 right-0 w-[50px] md:w-[75px] z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => {
-          const isKa = i % 2 === 0;
+          const currentIcon = iconData[i % iconData.length];
           return (
             <div key={`right-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
-              <img src={isKa ? "/ka-icon.png" : "/pizza-icon.png"} alt="" className="w-full h-full object-contain relative z-10" />
+              <img src={currentIcon.src} alt="" className="w-full h-full object-contain relative z-10" />
               <span className="absolute top-1/2 right-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none whitespace-nowrap">
-                {isKa ? 'karman' : 'pizza addict'}
+                {currentIcon.text}
               </span>
             </div>
           );
