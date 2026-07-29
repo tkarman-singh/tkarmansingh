4948fc1ea26416f6c4fe3814bca7ac387cc46b05
Karman Singh <t.karman.singh@gmail.com>
2026-07-29 21:19:08 +0530

    feat: make side borders scroll with page and fix navbar overlap

--- src/components/retro/SideBorders.tsx
+++ src/components/retro/SideBorders.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export function SideBorders() {
-  // 40 icons is enough to cover up to 2000px viewport height at 50px each.
+  // 200 icons is enough to cover up to 9000px height (at 45px each).
   // We don't use overflow-hidden on the container so the slide-out text can overflow sideways.
-  const icons = Array.from({ length: 40 });
+  const icons = Array.from({ length: 200 });
 
   return (
     <>
       {/* Left Border */}
-      <div className="fixed top-0 left-0 bottom-0 w-[30px] md:w-[45px] z-50 flex flex-col pointer-events-none">
+      <div className="absolute top-0 left-0 w-[30px] md:w-[45px] z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => (
           <div key={`left-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
             <img src="/ka-icon.png" alt="" className="w-full h-full object-contain relative z-10" />
@@ -20,7 +20,7 @@ export function SideBorders() {
       </div>
 
       {/* Right Border */}
-      <div className="fixed top-0 right-0 bottom-0 w-[30px] md:w-[45px] z-50 flex flex-col pointer-events-none">
+      <div className="absolute top-0 right-0 w-[30px] md:w-[45px] z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => (
           <div key={`right-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
             <img src="/ka-icon.png" alt="" className="w-full h-full object-contain relative z-10" />
