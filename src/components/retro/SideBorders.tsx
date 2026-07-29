b9a12dabd412be371e9eea01365a9476e4e84690
Karman Singh <t.karman.singh@gmail.com>
2026-07-29 21:26:28 +0530

    feat: alternate ka-icon with pizza-icon on side borders

--- src/components/retro/SideBorders.tsx
+++ src/components/retro/SideBorders.tsx
@@ -9,26 +9,32 @@ export function SideBorders() {
     <>
       {/* Left Border */}
       <div className="absolute top-0 left-0 w-[30px] md:w-[45px] z-[60] flex flex-col pointer-events-none">
-        {icons.map((_, i) => (
-          <div key={`left-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
-            <img src="/ka-icon.png" alt="" className="w-full h-full object-contain relative z-10" />
-            <span className="absolute top-1/2 left-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 -translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none">
-              karman
-            </span>
-          </div>
-        ))}
+        {icons.map((_, i) => {
+          const isKa = i % 2 === 0;
+          return (
+            <div key={`left-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
+              <img src={isKa ? "/ka-icon.png" : "/pizza-icon.png"} alt="" className="w-full h-full object-contain relative z-10 p-1" />
+              <span className="absolute top-1/2 left-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 -translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none whitespace-nowrap">
+                {isKa ? 'karman' : 'pizza addict'}
+              </span>
+            </div>
+          );
+        })}
       </div>
 
       {/* Right Border */}
       <div className="absolute top-0 right-0 w-[30px] md:w-[45px] z-[60] flex flex-col pointer-events-none">
-        {icons.map((_, i) => (
-          <div key={`right-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
-            <img src="/ka-icon.png" alt="" className="w-full h-full object-contain relative z-10" />
-            <span className="absolute top-1/2 right-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none">
-              karman
-            </span>
-          </div>
-        ))}
+        {icons.map((_, i) => {
+          const isKa = i % 2 === 0;
+          return (
+            <div key={`right-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
+              <img src={isKa ? "/ka-icon.png" : "/pizza-icon.png"} alt="" className="w-full h-full object-contain relative z-10 p-1" />
+              <span className="absolute top-1/2 right-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none whitespace-nowrap">
+                {isKa ? 'karman' : 'pizza addict'}
+              </span>
+            </div>
+          );
+        })}
       </div>
     </>
   );
