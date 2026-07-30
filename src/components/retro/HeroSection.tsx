6a80a2b4ad85731396292a494a45ab0f5a8dafdf
Karman Singh <t.karman.singh@gmail.com>
2026-07-31 01:42:11 +0530

    style: change language animation to fade in and out

--- src/components/retro/HeroSection.tsx
+++ src/components/retro/HeroSection.tsx
@@ -30,10 +30,10 @@ export function HeroSection() {
               <AnimatePresence mode="popLayout">
                 <motion.span
                   key={nameIndex}
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -20 }}
-                  transition={{ duration: 0.3 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
                   className="inline-block whitespace-nowrap"
                 >
                   {names[nameIndex]}
