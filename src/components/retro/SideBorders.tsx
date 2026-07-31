6572fceea5e2b4badfaacba7d36d25788e8cb88b
Karman Singh <t.karman.singh@gmail.com>
2026-07-31 12:21:56 +0530

    fix: remove overflow-hidden on side borders to allow text slide-out

--- src/components/retro/SideBorders.tsx
+++ src/components/retro/SideBorders.tsx
@@ -18,13 +18,13 @@ export function SideBorders() {
   return (
     <>
       {/* Left Border */}
-      <div className="absolute top-0 left-0 w-[50px] md:w-[75px] h-full overflow-hidden z-[60] flex flex-col pointer-events-none">
+      <div className="absolute top-0 left-0 w-[50px] md:w-[75px] h-full z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => {
           const currentIcon = iconData[i % iconData.length];
           return (
             <div key={`left-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
               <img src={currentIcon.src} alt="" className="w-full h-full object-contain relative z-10" />
-              <span className="absolute top-1/2 left-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 -translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none whitespace-nowrap">
+              <span className="absolute top-1/2 left-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 -translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-[60] tracking-wide pointer-events-none whitespace-nowrap">
                 {currentIcon.text}
               </span>
             </div>
@@ -33,13 +33,13 @@ export function SideBorders() {
       </div>
 
       {/* Right Border */}
-      <div className="absolute top-0 right-0 w-[50px] md:w-[75px] h-full overflow-hidden z-[60] flex flex-col pointer-events-none">
+      <div className="absolute top-0 right-0 w-[50px] md:w-[75px] h-full z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => {
           const currentIcon = iconData[i % iconData.length];
           return (
             <div key={`right-${i}`} className="relative w-full aspect-square group pointer-events-auto cursor-default shrink-0">
               <img src={currentIcon.src} alt="" className="w-full h-full object-contain relative z-10" />
-              <span className="absolute top-1/2 right-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-0 tracking-wide pointer-events-none whitespace-nowrap">
+              <span className="absolute top-1/2 right-[120%] -translate-y-1/2 font-kalam text-[#d351f7] font-bold text-[12px] md:text-[14px] opacity-0 translate-x-full transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0 z-[60] tracking-wide pointer-events-none whitespace-nowrap">
                 {currentIcon.text}
               </span>
             </div>
