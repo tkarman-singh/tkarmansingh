ad51bbe67331c6124795b47c8b7abb1134990f34
Karman Singh <t.karman.singh@gmail.com>
2026-07-31 12:53:15 +0530

    fix: use overflow-y-clip to prevent infinite scroll while allowing slide-out text to be visible

--- src/components/retro/SideBorders.tsx
+++ src/components/retro/SideBorders.tsx
@@ -18,10 +18,7 @@ export function SideBorders() {
   return (
     <>
       {/* Left Border */}
-      <div 
-        className="absolute top-0 left-0 w-[50px] md:w-[75px] h-full z-[60] flex flex-col pointer-events-none"
-        style={{ clipPath: 'inset(0 -500px 0 0)' }}
-      >
+      <div className="absolute top-0 left-0 w-[50px] md:w-[75px] h-full overflow-y-clip overflow-x-visible z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => {
           const currentIcon = iconData[i % iconData.length];
           return (
@@ -36,10 +33,7 @@ export function SideBorders() {
       </div>
 
       {/* Right Border */}
-      <div 
-        className="absolute top-0 right-0 w-[50px] md:w-[75px] h-full z-[60] flex flex-col pointer-events-none"
-        style={{ clipPath: 'inset(0 0 0 -500px)' }}
-      >
+      <div className="absolute top-0 right-0 w-[50px] md:w-[75px] h-full overflow-y-clip overflow-x-visible z-[60] flex flex-col pointer-events-none">
         {icons.map((_, i) => {
           const currentIcon = iconData[i % iconData.length];
           return (
